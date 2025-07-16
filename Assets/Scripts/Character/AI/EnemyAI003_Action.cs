316dd1415739d65f1636e98b61245558de061834
Ry-Seki <mcd2476094@stu.o-hara.ac.jp>
2025-07-16 14:45:43 +0900

    Seki_Fixed
    Fixed_CharacterUtility
    Fixed_EnemyAI

--- Assets/Scripts/Character/AI/EnemyAI003_Action.cs
+++ Assets/Scripts/Character/AI/EnemyAI003_Action.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using static CharacterUtility;
+
 public class EnemyAI003_Action : CharacterAIBase<EnemyCharacter> {
 
     public override void Setup() {
@@ -11,7 +13,11 @@ public class EnemyAI003_Action : CharacterAIBase<EnemyCharacter> {
 
     public override void Execute() {
         base.Execute();
+        float distance = PlayerToEnemyDistance();
 
+        //UŒ‚‚Å‚«‚éó‘Ô‚©‚Ç‚¤‚©
+
+        //
     }
 
     public override void Teardown() {
