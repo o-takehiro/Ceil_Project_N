0cdebf7714557b66ac805650ab4212f03ec81f19
Ry-Seki <mcd2476094@stu.o-hara.ac.jp>
2025-07-22 16:07:39 +0900

    Seki_Move
    Move_Any_File

--- Assets/Scripts/Character/AI/Enemy/EnemyAI002_CloseMove.cs
+++ Assets/Scripts/Character/AI/Enemy/EnemyAI002_CloseMove.cs
@@ -16,7 +16,20 @@ public class EnemyAI002_CloseMove : CharacterAIBase<EnemyCharacter> {
 
     public override void Execute() {
         base.Execute();
+        //自身と敵との距離
         float distance = PlayerToEnemyDistance();
+
+        //プレイヤーの方向を向き続ける
+        Quaternion enemyRotation = GetEnemyRotation();
+        //方向を決める
+        Vector3 direction = GetPlayerPosition() - GetEnemyPosition();
+        //水平方向のみの回転のため、yには0を代入
+        direction.y = 0;
+        //回転させる
+        Quaternion lookRotation = Quaternion.LookRotation(direction, Vector3.up);
+        enemyRotation = Quaternion.Slerp(enemyRotation, lookRotation, 0.1f);
+        //自身の回転に代入する
+        SetEnemyRotation(enemyRotation);
 
         Vector3 norm = (GetPlayerPosition() - GetEnemyPosition()).normalized;
         //y座標の移動を制限
