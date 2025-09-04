b852a7ffb513231f5dc09b0faf84d4c5c74faade
o-takehiro <mcd2476024@stu.o-hara.ac.jp>
2025-09-04 13:45:40 +0900

    oorui_fixdModel

--- Assets/Scripts/Character/Player/MageAnimationEvents.cs
+++ Assets/Scripts/Character/Player/MageAnimationEvents.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 
+using static CharacterUtility;
 public class MageAnimationEvents : MonoBehaviour {
     [SerializeField] private Collider attackCollider;
-    [SerializeField] private Renderer AttackRenderer;
+    //[SerializeField] private Renderer AttackRenderer;
     [SerializeField] private Animator animator;
+    // ダメージを与える相手
+    private string _TARGET_TAG = "Enemy";
     void Start() {
-        AttackRenderer.enabled = false;
+        //AttackRenderer.enabled = false;
         if (attackCollider != null) {
             attackCollider.enabled = false; // 初期状態はオフにしておく
         }
@@ -15,13 +18,24 @@ public class MageAnimationEvents : MonoBehaviour {
     public void OnCollider() {
         if (attackCollider != null) {
             attackCollider.enabled = true;
-            AttackRenderer.enabled = true;
+            //AttackRenderer.enabled = true;
         }
     }
     public void OffCollider() {
         if (attackCollider != null) {
             attackCollider.enabled = false;
-            AttackRenderer.enabled = false;
+            //AttackRenderer.enabled = false;
+        }
+    }
+
+    /// <summary>
+    /// ダメージを与える
+    /// </summary>
+    /// <param name="other"></param>
+    void OnTriggerEnter(Collider other) {
+        // タグで判定
+        if (other.CompareTag(_TARGET_TAG)) {
+            //ToEnemyDamage()
         }
     }
 
