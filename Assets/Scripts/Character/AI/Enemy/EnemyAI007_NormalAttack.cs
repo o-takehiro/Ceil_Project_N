19b508b1731976137c4a68c589ad05b5c6e21f9a
Ry-Seki <mcd2476094@stu.o-hara.ac.jp>
2025-09-05 14:25:51 +0900

    Seki
    Fixed_EnemyDeathAnimation

---
+++ Assets/Scripts/Character/AI/Enemy/EnemyAI007_NormalAttack.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+using static CharacterUtility;
+
+public class EnemyAI007_NormalAttack : CharacterAIBase<EnemyCharacter> {
+    public override void Initialize() {
+        base.Initialize();
+    }
+    public override void Setup() {
+        base.Setup();
+    }
+    public override void Execute() {
+        base.Execute();
+
+    }
+    public override void Teardown() {
+        base.Teardown();
+    }
+}
