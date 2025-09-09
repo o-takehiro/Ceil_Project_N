068fff5c8a6d44a2c992668e0d1cf4b51977726a
Ry-Seki <mcd2476094@stu.o-hara.ac.jp>
2025-09-09 12:21:13 +0900

    Seki
    Fixed_EnemyHP

--- Assets/Scripts/Character/AI/Enemy/EnemyAI005_Boss1Action.cs
+++ Assets/Scripts/Character/AI/Enemy/EnemyAI005_Boss1Action.cs
@@ -3,20 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem.DualShock;
 using static CharacterUtility;
+using static MagicUtility;
 
 public class EnemyAI005_Boss1Action : CharacterAIBase<EnemyCharacter> {
 
     public override void Setup() {
         base.Setup();
-
+        CancelAllEnemyMagic();
     }
     public override void Execute() {
         base.Execute();
         float distance = GetPlayerToEnemyDistance();
         if (distance > 50) {
             GetEnemy().myAI.ChangeState(new EnemyAI006_Charge());
+            AddEnemyMagicType(eMagicType.SatelliteOrbital);
+            CreateMagic(eSideType.EnemySide, GetEnemyMagicType(eMagicType.SatelliteOrbital));
         } else if (distance < 40 && distance > 5) {
             GetEnemy().myAI.ChangeState(new EnemyAI006_Charge());
+            AddEnemyMagicType(eMagicType.SatelliteOrbital);
+            CreateMagic(eSideType.EnemySide, GetEnemyMagicType(eMagicType.SatelliteOrbital));
         } else if (distance < 5) {
             GetEnemy().myAI.ChangeState(new EnemyAI007_NormalAttack());
         }  else {
