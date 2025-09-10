ecee7d554078086f7104b765248021ef7029ed43
o-takehiro <mcd2476024@stu.o-hara.ac.jp>
2025-09-10 11:34:31 +0900

    oorui_stageSize_up

--- Assets/Scripts/Stage/StageObject/goalObject.cs
+++ Assets/Scripts/Stage/StageObject/goalObject.cs
@@ -14,9 +14,23 @@ public class goalObject : StageObjectBase {
     }
 
     private void OnTriggerEnter(Collider collision) {
-        if(collision.gameObject.layer == 6) {
+        if (collision.gameObject.tag == "Player") {
             // SE再生
-                
+
+            // 遷移処理
+            eStageState stage = eStageState.Max;
+            if (stage == eStageState.Stage3) {
+                // タイトルに遷移
+                UniTask task = PartManager.Instance.TransitionPart(eGamePart.Title);
+            }
+            else if (stage == eStageState.Stage2) {
+                // [ステージ2]のとき[ステージ3]に
+                UniTask task = StageManager.Instance.TransitionStage(eStageState.Stage3);
+            }
+            else if (stage == eStageState.Stage1) {
+                // [ステージ1]のとき[ステージ2]に
+                UniTask task = StageManager.Instance.TransitionStage(eStageState.Stage2);
+            }
 
         }
     }
