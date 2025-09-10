637eb08abc11173aa14560baeaacb14a69dbe5f7
Ry-Seki <mcd2476094@stu.o-hara.ac.jp>
2025-09-10 11:11:03 +0900

    Seki
    Add_Boss2_Model

---
+++ Assets/Scripts/Character/Enemy/Boss2.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss2 : EnemyCharacter {
+    public override void Initialize() {
+        base.Initialize();
+    }
+
+    public override void Setup(int masterID) {
+        base.Setup(masterID);
+
+    }
+
+    public override void Teardown() {
+        base.Teardown();
+    }
+}
