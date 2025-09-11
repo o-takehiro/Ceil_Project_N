ff024b9ca280bc23df2efa9d923c349a9cf99e20
Ry-Seki <mcd2476094@stu.o-hara.ac.jp>
2025-09-11 11:12:07 +0900

    Seki
    Fixed_Boss1
    Fixed_Boss2

--- Assets/Scripts/Character/Enemy/Boss2.cs
+++ Assets/Scripts/Character/Enemy/Boss2.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 using static GameConst;
 using static CharacterUtility;
-using static CharacterMasterUtility;
 
 public class Boss2 : EnemyCharacter {
     public override void Initialize() {
@@ -17,21 +16,7 @@ public class Boss2 : EnemyCharacter {
 
     public override void Setup(int masterID) {
         base.Setup(masterID);
-        var characterMaster = GetCharacterMaster(masterID);
-        SetMaxHP(characterMaster.HP);
-        SetHP(characterMaster.HP);
-        SetRawAttack(characterMaster.Attack);
-        SetRawDefense(characterMaster.Defense);
-        //現在の位置更新
-        SetEnemyPosition(transform.position);
-        //一フレーム前の位置更新
-        SetEnemyPrevPosition();
-        //中心座標更新
-        SetEnemyCenterPosition(new Vector3(transform.position.x, transform.position.y + 2, transform.position.z));
-        //現在の回転更新
-        SetEnemyRotation(transform.rotation);
-        //HPゲージの設定
-        SetupCanvasPosition(Vector3.one * 3);
+
         myAI.Setup(this);
         myAI.ChangeState(new EnemyAI001_Wait());
     }
