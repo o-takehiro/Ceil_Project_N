81774f6f1f0290771762d04cd0160a2fcb665e0f
Ry-Seki <mcd2476094@stu.o-hara.ac.jp>
2025-09-18 11:55:03 +0900

    Seki
    Fixed_EnemyHPGauge

--- Assets/Scripts/Character/Enemy/EnemyCharacter.cs
+++ Assets/Scripts/Character/Enemy/EnemyCharacter.cs
@@ -75,10 +75,9 @@ public class EnemyCharacter : CharacterBase {
         enemyHPGauge = MenuManager.Instance.Get<EnemyHPGauge>().GetSlider();
     }
 
-    protected void SetupCanvasPosition(Vector3 setSize) {
+    protected virtual void SetupCanvasPosition(Vector3 setSize) {
         Vector3 canvasPos = Vector3.zero;
         SetEnemyCanvas();
-        enemyHPGauge.transform.SetParent(enemyCanvas.transform);
         enemyHPGauge.transform.SetParent(enemyCanvas.transform, false);
         enemyHPGauge.transform.localPosition = Vector3.zero;
         enemyHPGauge.transform.localRotation = Quaternion.Euler(0, 180f, 0); // –ˆ‰ñ180“x‚ÉŠm’è
